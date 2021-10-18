7fa9015ff63b2e6911ddfe7f1c9a286687545810
Chief A <aathilr7@gmail.com>
2021-10-18 13:34:06 +0530

    Complex numbers in python.

--- Basics/NumericalComputations.py
+++ Basics/NumericalComputations.py
@@ -1,3 +1,6 @@
 print(1.5//1) #Removes the fractional part of result.
 print (2**3)  # 2 to the power 3  
 print(3/2)    # division always returns a floating point number
+a=1+2j         # python has inbuilt support for complex numbers
+b=3+4j
+print(a+b)      # 4+6j
